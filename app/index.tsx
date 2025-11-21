6c3e4258f35d1fce6c97e88dce89844b096d7f39
Phillip Paul <ppaul_dev@t-online.de>
2025-11-22 00:25:09 +0100

    feat: add onboarding screen with navigation link

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-5xl text-dark-200 font-bold">Hello World!</Text>
+      <Link href="/onboarding">Onboarding</Link>
     </View>
   );
 }
