4e5efbbc79ba5010d372b86c3e145677c1a0e642
Phillip Paul <ppaul_dev@t-online.de>
2025-11-21 23:40:09 +0100

    config: configure Tailwind content paths and NativeWind preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
