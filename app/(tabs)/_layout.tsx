c20c46acf74870e47d83c622f3c99887fde6626a
Phillip Paul <ppaul_dev@t-online.de>
2025-11-22 16:38:18 +0100

    feat: enhance tab navigation by adding custom tab bar icon for Home screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,10 @@
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
 import { Tabs } from 'expo-router'
 import React from 'react'
+import { Image, ImageBackground, Text } from 'react-native'
 
-const _layout = () => {
+const _Layout = () => {
   return (
     <Tabs>
       <Tabs.Screen
@@ -9,6 +12,20 @@ const _layout = () => {
         options={{
           title: 'Home',
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <ImageBackground
+                source={images.highlight}
+                className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+              >
+                <Image source={icons.home}
+                  tintColor="#151312" />
+                  <Text
+                    className="text-secondary text-base font-semibold ml-2"
+                  >Home</Text>
+              </ImageBackground>
+            </>
+          ),
         }}
       />
       <Tabs.Screen
@@ -36,4 +53,4 @@ const _layout = () => {
   )
 }
 
-export default _layout
+export default _Layout
