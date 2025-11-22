fbb8fa4a63b5b479ab105654ccb18b88c1d41614
Phillip Paul <ppaul_dev@t-online.de>
2025-11-22 16:00:25 +0100

    refactor: remove onboarding and movie detail components, update index screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,9 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-5xl text-dark-200 font-bold">Hello World!</Text>
-      <Link href="/onboarding">Onboarding</Link>
-      <Link href="/movie/avengers">Avengers Movie</Link>
     </View>
   );
 }
