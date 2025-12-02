436534f96b37170d32d7c6c53a4af9bfc8ca37a1
Phillip Paul <ppaul_dev@t-online.de>
2025-12-02 16:22:14 +0100

    refactor: rename profile and saved components to follow PascalCase convention and remove unnecessary text elements

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,16 +2,15 @@ import { icons } from '@/constants/icons'
 import React from 'react'
 import { Image, Text, View } from 'react-native'
 
-const profile = () => {
+const Profile = () => {
   return (
     <View className="bg-primary flex-1 px-10">
       <View className="flex justify-center items-center flex-1 flex-col gap-5">
         <Image source={icons.person} className="size-10" tintColor="#fff" />
         <Text className="text-gray-500 text-base">Profile</Text>
       </View>
-      <Text>profile</Text>
     </View>
   )
 }
 
-export default profile
+export default Profile
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -2,16 +2,15 @@ import { icons } from '@/constants/icons'
 import React from 'react'
 import { Image, Text, View } from 'react-native'
 
-const saved = () => {
+const Saved = () => {
   return (
     <View className="bg-primary flex-1 px-10">
     <View className="flex justify-center items-center flex-1 flex-col gap-5">
       <Image source={icons.save} className="size-10" tintColor="#fff" />
       <Text className="text-gray-500 text-base">Save</Text>
     </View>
-    <Text>profile</Text>
   </View>
   )
 }
 
-export default saved
+export default Saved
