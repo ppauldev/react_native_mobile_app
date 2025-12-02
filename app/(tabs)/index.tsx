e5ab4742759ad316899474318e043b3700cbb067
Phillip Paul <ppaul_dev@t-online.de>
2025-12-02 14:02:17 +0100

    feat: add TrendingCard component and integrate it into the index screen to display trending movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
+import TrendingCard from "@/components/TrendingCard";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
@@ -53,6 +54,24 @@ export default function Index() {
               value=""
               onChangeText={() => {}}
             />
+            {trendingMovies && (
+              <View className="mt-10">
+                <Text className="text-lg text-white font-bold mb-3">Trending Movies</Text>
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  ItemSeparatorComponent={() => <View className="w-4" />}
+                  className="mb-4 mt-3"
+                  data={trendingMovies}
+                  renderItem={({ item, index }) => (
+                    <Text className="text-white text-sm">
+                      <TrendingCard movie={item} index={index} />
+                    </Text>
+                  )}
+                  keyExtractor={(item) => item.movie_id.toString()}
+                  />
+              </View>
+            )}
             <>
               <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
               <FlatList
